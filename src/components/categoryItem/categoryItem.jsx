c607c257fec6506b504814f78e6c625123511611
milkiase <milktes27@gmail.com>
2023-08-26 17:18:19 +0300

    Made the home page shop now links to work

--- src/components/categoryItem/categoryItem.jsx
+++ src/components/categoryItem/categoryItem.jsx
@@ -1,10 +1,15 @@
-import './categoryItem.styles.scss'
+import { useNavigate } from 'react-router-dom';
+import './categoryItem.styles.scss';
 const CategoryItem = ({category: {title, imgURL}})=>{
+    const navigator = useNavigate()
+    const shopNow = (goTo)=>{
+        navigator(`shop/${goTo.toLowerCase()}`)
+    }
     return (
         <div className="category-item-container" style={{
             backgroundImage: `url(${imgURL})`,
         }}>
-            <div className="category-body-container" >
+            <div className="category-body-container" onClick={()=>{shopNow(title)}}>
                 <h2>{title.toUpperCase()}</h2>
                 <p>SHOP NOW</p>
             </div>
