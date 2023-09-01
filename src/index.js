f9264ad11ba8d5aa1ac836c08135eb7131c731c8
milkiase <milktes27@gmail.com>
2023-09-01 05:27:21 +0300

    implemented the react-persist library to persist the store in localStorage

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.scss';
 import App from './App';
-import {store} from './store/store'
+import {persistor, store} from './store/store'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,19 @@
-import {createStore, compose, applyMiddleware} from 'redux'
+import {createStore, compose, applyMiddleware} from 'redux';
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {logger} from 'redux-logger'
 import rootReducer from './rootReducer'
 
-const composeEnhancer = compose(applyMiddleware(logger))
-export const store = createStore(rootReducer, undefined, composeEnhancer)
+
+const persistConfig = {
+    key : 'root',
+    storage,
+    blacklist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const middlewares = [logger];
+const composeEnhancer = compose(applyMiddleware(...middlewares));
+export const store = createStore(persistedReducer, undefined, composeEnhancer);
+
+export const persistor = persistStore(store);
